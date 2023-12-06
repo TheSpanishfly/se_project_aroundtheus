08134b1e9a54aa6e711e3f5a2d8810ade0aa7983
AlexPerez <alexperez123@gmail.com>
2023-12-06 15:18:54 -0500

    updated index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import index from "../pages/index.css";
 import Card from "../components/Card.js";
 import FormValidator from "../components/FormValidator.js";
 import Section from "../components/Section.js";
