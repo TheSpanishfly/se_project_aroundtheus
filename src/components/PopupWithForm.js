d53e7318605def4df179992738fa09fc579f6409
AlexPerez <alexperez123@gmail.com>
2023-12-07 13:37:10 -0500

    updated PopupWithForm.js

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -25,9 +25,6 @@ export default class PopupWithForm extends Popup {
       evt.preventDefault();
       this._handleFormSubmit(this._getInputValues());
     });
-    this._closeButton.addEventListener("click", () => {
-      this.close();
-    });
     super.setEventListeners();
   }
 
